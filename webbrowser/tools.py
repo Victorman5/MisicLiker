f1ac028b30d578c64e9ede2a9fff9b862030ad83
Victorman5 <53015676+Victorman5@users.noreply.github.com>
2021-03-13 16:35:20 +0300

    2nd part update

--- webbrowser/tools.py
+++ webbrowser/tools.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 def universal_click(driver, element):
     ActionChains(driver).move_to_element(element).click().perform()
     driver.implicitly_wait(2)
+
+
+def scroll_to(driver, x, y):
+    driver.execute_script(f'window.scrollTo({x}, {y})')
